5f48df844f6fab7553892b94b365714f752de0ff
Darker <1918891542@qq.com>
2020-03-13 21:53:45 +0800

    Update local.py

--- local.py
+++ local.py
@@ -2,6 +2,8 @@ import socket
 import select
 import queue
 import encryption
+import threading
+
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server.setblocking(False)
@@ -11,6 +13,7 @@ inputs = [server]
 outputs = []
 message_queues = {}
 connection = {}
+send_list = set()
 
 def clear(c):
 	inputs.remove(c)
@@ -29,6 +32,35 @@ def corr(dic, c1, c2):
 	dic[c2] = c1
 	return dic
 
+def send(s):
+	try:
+		while True:
+			if s not in send_list:
+				break
+		send_list.add(s)		
+		message_queue = message_queues.get(s)
+		if message_queue == None:
+			send_list.discard(s)
+			return
+		send_data = ''					
+		if not message_queue.empty():
+			send_data = message_queue.get_nowait()
+			if b'CONNECT' in send_data:
+				send_data = encryption.simple_e_b64(send_data)	
+			#print('发送数据')
+			s.sendall(send_data)
+			#print('数据发送成功！')
+		else:
+			if s in outputs:
+				outputs.remove(s)
+		send_list.discard(s)
+
+	except Exception as e:
+		print('获取数据出现错误:' + str(e))
+		if s in outputs:
+			outputs.remove(s)
+		send_list.discard(s)
+
 while True:
 	# print ('waiting for the next event')
 	readable, writable, exceptional = select.select(inputs, outputs, inputs)
@@ -42,7 +74,7 @@ while True:
 			# dst_conn.setblocking(0)
 			try:
 				# 目标服务器IP和端口
-				dst_conn.connect(('192.168.247.135', 8888))
+				dst_conn.connect(('127.0.0.1', 8888))
 			except Exception as e:
 				print('连接服务器出现错误＞︿＜' + str(e))
 				continue
@@ -70,24 +102,4 @@ while True:
 					clear(s)	
 
 	for s in writable:
-		try:
-			message_queue = message_queues.get(s)
-			if message_queue == None:
-				break
-			send_data = ''					
-			if not message_queue.empty():
-				send_data = message_queue.get_nowait()
-				#print('收到数据:'+str(send_data))
-				if b'CONNECT' in send_data:
-					send_data = encryption.simple_e_b64(send_data)
-					pass
-				#print('发送数据:'+str(send_data))
-				s.send(send_data)
-				#print('数据发送成功！')
-			else:
-				outputs.remove(s)
-
-		except Exception as e:
-			print('获取数据出现错误:' + str(e))
-			if s in outputs:
-				outputs.remove(s)
+		threading.Thread(target=send, args=(s,)).start()
